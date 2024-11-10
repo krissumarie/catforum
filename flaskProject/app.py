65eeb5a2e1cb017765c15e1ab634313b09a2734f
Kristiina Marie Palu <krpalh@taltech.ee>
2024-11-10 19:53:24 +0200

    Database katse

--- flaskProject/app.py
+++ flaskProject/app.py
@@ -1,5 +1,11 @@
 from flask import Flask
 from flask import render_template
+from flask import request
+from flaskProject.database.database import create_database
+from flask import session, flash
+from flask import Flask, redirect
+
+from flaskProject.database.user import create_user
 
 app = Flask(__name__)
 
@@ -15,6 +21,18 @@ def profiiliseaded():
 @app.route('/enda_konto')
 def enda_konto():
     return render_template('enda_konto.html')
+
+# app.config.from_object('config:Config')
+with app.app_context(): # must be in application context to execute
+    create_database()
+
+
+@app.route('/signup', methods=["POST"])
+def signup():
+    name = request.form.get('name')
+    password = request.form.get('password')
+    create_user(name, password)
+    return redirect("/")
 
 
 if __name__ == '__main__':
