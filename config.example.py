a29be809c93cccbb2a985f7524f8e0a5d60937d4
Kaur Drenkhan <kadren@taltech.ee>
2024-11-18 19:37:05 +0200

    config.example fail lisamine

---
+++ config.example.py
@@ -0,0 +1,5 @@
+class Config:
+    # Enter your OpenWeather API key here
+    OPEN_WEATHER_API_KEY = "<your_secret_key>"
+    # Connection string used for postgres database access.'your_secret_key'
+    POSTGRES_CONNECTION_STRING = "host=localhost user=postgres password=postgres port=5432 dbname=postgres"
